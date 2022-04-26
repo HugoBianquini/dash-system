25ee58ea71e6a27288a8225e65358339b834cf78
hugobianquini <hugobianqui@hotmail.com>
2022-04-25 23:06:43 -0300

    fixing files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { AppProps } from 'next/app'
-import { theme } from '../styles/theme'
+import { ChakraProvider } from '@chakra-ui/react';
+import { AppProps } from 'next/app';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <ChakraProvider resetCSS theme={theme}>
-    <Component {...pageProps} />
-  </ChakraProvider>)
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;

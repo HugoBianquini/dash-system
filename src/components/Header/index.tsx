386820fc198629b8ac117a83e8a2ca0992acf877
hugobianquini <hugobianqui@hotmail.com>
2022-08-06 18:57:42 -0300

    make sidebar responsive and creating drawercontext

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContent';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
   const isDesktop = useBreakpointValue({ base: false, lg: true });
   return (
     <Flex
@@ -17,6 +20,16 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isDesktop && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="un"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
       <Logo />
       {isDesktop && <SearchBox />}
       <Flex align="center" ml="auto">

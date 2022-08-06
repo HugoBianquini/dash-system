1fbf585ddf73018986b7a1e07f1ab89e46892e87
hugobianquini <hugobianqui@hotmail.com>
2022-08-06 19:38:04 -0300

    Fix graphics bug

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react';
 import dynamic from 'next/dynamic';
 import { ApexOptions } from 'apexcharts';
 
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
 
@@ -65,31 +66,48 @@ const series = [
 ];
 
 export default function Dashboard() {
+  const [mountGraphics, setMountGraphics] = useState(false);
+
+  useEffect(() => {
+    setMountGraphics(true);
+  }, []);
+
   return (
     <Flex direction="column" h="100vh">
       <Header />
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <Sidebar />
-
-        <SimpleGrid
-          flex="1"
-          gap="4"
-          minChildWidth="320px"
-          alignItems="flex-start"
-        >
-          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
-              Inscritos da semana
-            </Text>
-            <Chart options={options} series={series} type="area" height={160} />
-          </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
-              Taxa de Abertura
-            </Text>
-            <Chart options={options} series={series} type="area" height={160} />
-          </Box>
-        </SimpleGrid>
+        {mountGraphics && (
+          <SimpleGrid
+            flex="1"
+            gap="4"
+            minChildWidth="320px"
+            alignItems="flex-start"
+          >
+            <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
+              <Text fontSize="lg" mb="4">
+                Inscritos da semana
+              </Text>
+              <Chart
+                options={options}
+                series={series}
+                type="area"
+                height={160}
+              />
+            </Box>
+            <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
+              <Text fontSize="lg" mb="4">
+                Taxa de Abertura
+              </Text>
+              <Chart
+                options={options}
+                series={series}
+                type="area"
+                height={160}
+              />
+            </Box>
+          </SimpleGrid>
+        )}
       </Flex>
     </Flex>
   );

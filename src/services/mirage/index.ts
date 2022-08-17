47e7d576d6b72f1f77f52fa73cb27352c292125b
hugobianquini <hugobianqui@hotmail.com>
2022-08-17 19:29:29 -0300

    installing and configuring mirage and fakerjs

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,49 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable camelcase */
+import { createServer, Factory, Model } from 'miragejs';
+import { faker } from '@faker-js/faker';
+
+type User = {
+  name: string;
+  email: string;
+  created_at: string;
+};
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+    },
+
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i + 1}`;
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      }),
+    },
+
+    seeds(serv) {
+      serv.createList('user', 200);
+    },
+
+    routes() {
+      this.namespace = 'api';
+      this.timing = 750;
+
+      this.get('/users');
+      this.post('/users');
+
+      this.namespace = '';
+      this.passthrough();
+    },
+  });
+
+  return server;
+}
